e82ff74074160bc0eeaec70e128ed9ac9499b78b
derekepark <79765656+derekepark@users.noreply.github.com>
2021-05-15 21:09:00 -0400

    Add files via upload
    GUI now displays the board
    Board Representation in main App.java file
    images of the pieces that will be used in the GUI

--- engine/Engine/src/App.java
+++ engine/Engine/src/App.java
@@ -2,13 +2,25 @@ import javax.swing.*; //new commands and added functionality
 
 
 public class App {
+
+
+    static char board[][]=
+    {
+        {'r','n','b','q','k','b','n','r'},
+        {'p','p','p','p','p','p','p','p'},
+        {' ',' ',' ',' ',' ',' ',' ',' '},
+        {' ',' ',' ',' ',' ',' ',' ',' '},
+        {' ',' ',' ',' ',' ',' ',' ',' '},
+        {' ',' ',' ',' ',' ',' ',' ',' '},
+        {'P','P','P','P','P','P','P','P'},
+        {'R','N','B','Q','K','B','N','R'}};
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         JFrame UI=new JFrame("Derek Park's Chess Engine");
         UI.setDefaultCloseOperation((JFrame.EXIT_ON_CLOSE)); //closes the UI when you close the window
         UserInterface ui=new UserInterface(); 
         UI.add(ui);
-        UI.setSize(600, 600); 
+        UI.setSize(528, 550); 
         UI.setVisible(true);
 
     }
--- engine/Engine/src/UserInterface.java
+++ engine/Engine/src/UserInterface.java
@@ -8,52 +8,76 @@ public class UserInterface extends JPanel implements MouseInputListener
 {
     static int x=0;//x cord
     static int y=0;//y cord 
+    static int tileSize=64;
     public void paintComponent(Graphics g)
     {   
         super.paintComponent(g); //need this to set the background
         this.setBackground(Color.gray);
         this.addMouseListener(this); //need this to track mouse inputs
         this.addMouseMotionListener(this); //this for mouse movement
-        g.setColor(Color.black); //order of the colors matter!
-        g.fillRect(10,20,20,20);
-        g.setColor(Color.pink);
-        g.fillRect(20,40,20,20); //pink will always be on top of black since it order of colors
+        for (int i=0; i<64; i+=2) //create the board
+        {
+            g.setColor(new Color(235,243,243));
+            g.fillRect((i%8+(i/8)%2) *tileSize, (i/8)*tileSize, tileSize,tileSize);
+            g.setColor(new Color(0,121,191));
+            g.fillRect(((i+1)%8-((i+1)/8)%2) *tileSize, (i/8)*tileSize, tileSize,tileSize);
+
+        }
+
+
+
+
+
+
+    /*  Image king= new ImageIcon("king.jpg").getImage();
+      Image bking= new ImageIcon("bking.jpg").getImage();
+      Image queen= new ImageIcon("queen.jpg").getImage();
+      Image bqueen= new ImageIcon("bqueen.jpg").getImage();
+      Image bishop= new ImageIcon("bishop.jpg").getImage();
+      Image bbishop= new ImageIcon("bbishop.jpg").getImage();
+      Image knight= new ImageIcon("knight.jpg").getImage();
+      Image bknight= new ImageIcon("bknight.jpg").getImage();
+      Image pawn= new ImageIcon("pawn.jpg").getImage();
+      Image bpawn= new ImageIcon("bpawn.jpg").getImage();
+      Image rook= new ImageIcon("rook.jpg").getImage();
+      Image brook= new ImageIcon("brook.jpg").getImage();
+        */
     }
     
-    
+   
+
     public void mouseClicked(java.awt.event.MouseEvent e) {
-        // TODO Auto-generated method stub
           
     }
    
     public void mousePressed(java.awt.event.MouseEvent e) {
         // TODO Auto-generated method stub
-        
+        x=e.getX();
+        y=e.getY();
+        repaint();
     }
   
     public void mouseReleased(java.awt.event.MouseEvent e) {
         // TODO Auto-generated method stub
-        
+        x=e.getX();
+        y=e.getY();
+        repaint();
     }
     
     public void mouseEntered(java.awt.event.MouseEvent e) {
-        // TODO Auto-generated method stub
-        
+     
     }
     
     public void mouseExited(java.awt.event.MouseEvent e) {
-        // TODO Auto-generated method stub
-        
+
     }
    
     public void mouseDragged(java.awt.event.MouseEvent e) {
-        // TODO Auto-generated method stub
-        
+    
     }
    
     public void mouseMoved(java.awt.event.MouseEvent e) {
-        // TODO Auto-generated method stub
-        
+    
     }
 
 
